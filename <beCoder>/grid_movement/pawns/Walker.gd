2ada4e7846d7478273e7c347394212e6d850dccf
Evgeny Bukreev <54906422+eugenpolytechnic@users.noreply.github.com>
2020-08-02 13:21:13 +0300

    Add files via upload

---
+++ <beCoder>/grid_movement/pawns/Walker.gd
@@ -0,0 +1,43 @@
+extends 'Pawn.gd'
+
+onready var Grid = get_parent()
+#warning-ignore:unused_class_variable
+export (PackedScene) var combat_actor
+#warning-ignore:unused_class_variable
+var lost = false
+
+
+func _process(_delta):
+	var input_direction = get_input_direction()
+	if not input_direction:
+		return
+
+	var target_position = Grid.request_move(self, input_direction)
+	if target_position:
+		move_to(target_position)
+		$Tween.start()
+	else:
+		bump()
+
+
+func get_input_direction():
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+func move_to(target_position):
+	set_process(false)
+	$AnimationPlayer.play("walk")
+	var move_direction = (position - target_position).normalized()
+	$Tween.interpolate_property($Pivot, "position", move_direction * 32, Vector2(), $AnimationPlayer.current_animation_length, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Pivot/Sprite.position = position - target_position
+	position = target_position
+	
+	yield($AnimationPlayer, "animation_finished")
+	
+	set_process(true)
+
+
+func bump():
+	$AnimationPlayer.play("bump")
